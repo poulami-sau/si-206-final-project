3b40b6236524a8aedc2298d53cc800ec6d066fef
poulami-sau <109125687+poulami-sau@users.noreply.github.com>
2023-11-25 00:30:37 -0500

    updated weather_info.py to account for inserting 25 or less rows at a time and updated weather_data table to include zip_code_id

--- weather_info.py
+++ weather_info.py
@@ -9,7 +9,7 @@ db_filename = dir_path + '/zipCode_airQuality.db'
 conn = sqlite3.connect(db_filename)
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS weather_info_data (zip_code TEXT, date_val TEXT, hour_gmt_val INTEGER, temperature REAL, relative_humidity INTEGER, precipitation REAL, cloud_cover INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS weather_data (zip_code_id INTEGER, date_val TEXT, hour_gmt_val INTEGER, temperature REAL, relative_humidity INTEGER, precipitation REAL, cloud_cover INTEGER, FOREIGN KEY (zip_code_id) REFERENCES zip_code_data(zip_code_id))")
 
 open_meteo_url = "https://api.open-meteo.com/v1/forecast?"
 params = {}
@@ -22,29 +22,38 @@ params["forecast_days"] = 1
 
 date_yesterday = date.today() - timedelta(days = 1)
 
-cur.execute("SELECT * FROM zip_code_to_coordinates")
-coordinates = cur.fetchall()
+cur.execute("SELECT MAX(zip_code_id) FROM alt_weather_info_data WHERE date_val = ?", (date_yesterday,))
+max_zip_code_id = cur.fetchone()[0]
 
-for zip_code, latitude, longitude in coordinates:
-    params["latitude"] = latitude
-    params["longitude"] = longitude
-    response = requests.get(open_meteo_url, params=params)
-    weather_info = json.loads(response.text)
+if max_zip_code_id == None:
+    max_zip_code_id = 1
+elif max_zip_code_id < 100:
+    max_zip_code_id += 1
+elif max_zip_code_id == 100:
+    exit()
 
-    yesterday_info_list = weather_info["hourly"]
+cur.execute("SELECT * FROM zip_code_to_coordinates where zip_code_id = ?", (max_zip_code_id,))
+zip_code_id, latitude, longitude = cur.fetchone()
 
-    for hour in range(24):
-        yesterday_timestamp = yesterday_info_list["time"][hour]
-        yesterday_timestamp_hour = yesterday_timestamp[11:13]
+params["latitude"] = latitude
+params["longitude"] = longitude
+response = requests.get(open_meteo_url, params=params)
+weather_info = json.loads(response.text)
 
-        yesterday_temperature = yesterday_info_list["temperature_2m"][hour]
-        yesterday_relative_humidity = yesterday_info_list["relative_humidity_2m"][hour]
-        yesterday_precipitation = yesterday_info_list["precipitation"][hour]
-        yesterday_cloud_cover = yesterday_info_list["cloud_cover"][hour]
+yesterday_info_list = weather_info["hourly"]
 
-        cur.execute("INSERT OR IGNORE INTO weather_info_data (zip_code, date_val, hour_gmt_val, temperature, relative_humidity, precipitation, cloud_cover) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                (zip_code, date_yesterday, yesterday_timestamp_hour, yesterday_temperature, yesterday_relative_humidity, yesterday_precipitation, yesterday_cloud_cover))
-        
+for hour in range(24):
+    yesterday_timestamp = yesterday_info_list["time"][hour]
+    yesterday_timestamp_hour = yesterday_timestamp[11:13]
+
+    yesterday_temperature = yesterday_info_list["temperature_2m"][hour]
+    yesterday_relative_humidity = yesterday_info_list["relative_humidity_2m"][hour]
+    yesterday_precipitation = yesterday_info_list["precipitation"][hour]
+    yesterday_cloud_cover = yesterday_info_list["cloud_cover"][hour]
+
+    cur.execute("INSERT OR IGNORE INTO weather_data (zip_code_id, date_val, hour_gmt_val, temperature, relative_humidity, precipitation, cloud_cover) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (zip_code_id, date_yesterday, yesterday_timestamp_hour, yesterday_temperature, yesterday_relative_humidity, yesterday_precipitation, yesterday_cloud_cover))
+
 conn.commit()
 
 cur.close()
