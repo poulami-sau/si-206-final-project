4a13e20e4f1f4c5604ebcad19ff311234bff3096
poulami-sau <109125687+poulami-sau@users.noreply.github.com>
2023-11-30 11:38:00 -0500

    edited header list

--- calculated_data.py
+++ calculated_data.py
@@ -7,18 +7,14 @@ db_filename = dir_path + '/zipCode_airQuality.db'
 conn = sqlite3.connect(db_filename)
 cur = conn.cursor()
 
-cur.execute("CREATE TEMPORARY TABLE joined_weather_info AS SELECT state, AVG(temperature) AS avg_temperature, AVG(relative_humidity)" 
-            "AS avg_relative_humidity, AVG(precipitation) AS avg_precipitation, AVG(cloud_cover) AS avg_cloud_cover FROM weather_data" 
-            "JOIN zip_code_data ON weather_data.zip_code_id = zip_code_data.zip_code_id GROUP BY state")
+cur.execute("""CREATE TEMPORARY TABLE joined_weather_info AS SELECT state, AVG(temperature) AS avg_temperature, AVG(relative_humidity) AS avg_relative_humidity, AVG(precipitation) AS avg_precipitation, AVG(cloud_cover) AS avg_cloud_cover FROM weather_data JOIN zip_code_data ON weather_data.zip_code_id = zip_code_data.zip_code_id GROUP BY state""")
 
-cur.execute("CREATE TEMPORARY TABLE joined_aqi_info AS SELECT state, AVG(aqi) AS avg_aqi FROM air_quality_data JOIN zip_code_data"
-            "ON air_quality_data.zip_code_id = zip_code_data.zip_code_id GROUP BY state")
+cur.execute("CREATE TEMPORARY TABLE joined_aqi_info AS SELECT state, AVG(aqi) AS avg_aqi FROM air_quality_data JOIN zip_code_data ON air_quality_data.zip_code_id = zip_code_data.zip_code_id GROUP BY state")
 
-cur.execute("SELECT joined_weather_info.state, avg_temperature, avg_relative_humidity, avg_precipitation, avg_cloud_cover,"
-            "avg_aqi FROM joined_weather_info JOIN joined_aqi_info ON joined_weather_info.state = joined_aqi_info.state")
+cur.execute("SELECT joined_weather_info.state, avg_temperature, avg_relative_humidity, avg_precipitation, avg_cloud_cover, avg_aqi FROM joined_weather_info JOIN joined_aqi_info ON joined_weather_info.state = joined_aqi_info.state")
 results = cur.fetchall()
 
-header_list = ["state", "avg_temperature", "avg_relative_humidity", "avg_precipitation", "avg_cloud_cover", "avg_aqi"]
+header_list = ["state", "avg_weekly_temperature", "avg_weekly_relative_humidity", "avg_weekly_precipitation", "avg_weekly_cloud_cover", "avg_weekly_aqi"]
 fout = open("calculations.csv", "w")
 
 writer = csv.writer(fout)
