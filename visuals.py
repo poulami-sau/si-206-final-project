19f1ae31002610f1de6141a06be4e9afa1be4555
Kate Chung <katechung@MacBook-Air-726.local>
2023-11-30 11:33:19 -0500

    updated visuals to top 50

--- visuals.py
+++ visuals.py
@@ -30,17 +30,17 @@ for num in data[1:]:
 def create_temperature_graph():
     sort_by_temperature = sorted(num_data, key = lambda x: x[1])
 
-    top_10_temperature = []
+    top_50_temperature = []
     top_aqi = []
-    for index in range(len(sort_by_temperature) - 10, len(sort_by_temperature)):
-        top_10_temperature.append(sort_by_temperature[index][1])
+    for index in range(len(sort_by_temperature) - 50, len(sort_by_temperature)):
+        top_50_temperature.append(sort_by_temperature[index][1])
         top_aqi.append(sort_by_temperature[index][5])
 
     fig1, ax1 = plt.subplots()
-    ax1.scatter(top_10_temperature, top_aqi)
-    ax1.set_xlabel("average temperature")
-    ax1.set_ylabel("average aqi")
-    ax1.set_title("Average AQI vs Top 10 Average Temperature (by State)")
+    ax1.scatter(top_50_temperature, top_aqi)
+    ax1.set_xlabel("average weekly temperature")
+    ax1.set_ylabel("average weekly aqi")
+    ax1.set_title("Average AQI vs Top 50 Average Temperature (by State)")
 
     fig1.savefig("temperature.png")
 
@@ -48,17 +48,17 @@ def create_temperature_graph():
 def create_humidity_graph():
     sort_by_humidity = sorted(num_data, key = lambda x: x[2])
 
-    top_10_humidity = []
+    top_50_humidity = []
     top_aqi = []
-    for index in range(len(sort_by_humidity) - 10, len(sort_by_humidity)):
-        top_10_humidity.append(sort_by_humidity[index][2])
+    for index in range(len(sort_by_humidity) - 50, len(sort_by_humidity)):
+        top_50_humidity.append(sort_by_humidity[index][2])
         top_aqi.append(sort_by_humidity[index][5])
 
     fig2, ax2 = plt.subplots()
-    ax2.scatter(top_10_humidity, top_aqi)
-    ax2.set_xlabel("average relative_humidity")
-    ax2.set_ylabel("average aqi")
-    ax2.set_title("Average AQI vs Top 10 Average Relative Humidity (by State)")
+    ax2.scatter(top_50_humidity, top_aqi)
+    ax2.set_xlabel("average weekly relative humidity")
+    ax2.set_ylabel("average weekly aqi")
+    ax2.set_title("Average AQI vs Top 50 Average Relative Humidity (by State)")
 
     fig2.savefig("humidity.png")
 
@@ -66,17 +66,17 @@ def create_humidity_graph():
 def create_precipitation_graph():
     sort_by_precipitation = sorted(num_data, key = lambda x: x[3])
 
-    top_10_precipitation = []
+    top_50_precipitation = []
     top_aqi = []
-    for index in range(len(sort_by_precipitation) - 10, len(sort_by_precipitation)):
-        top_10_precipitation.append(sort_by_precipitation[index][3])
+    for index in range(len(sort_by_precipitation) - 50, len(sort_by_precipitation)):
+        top_50_precipitation.append(sort_by_precipitation[index][3])
         top_aqi.append(sort_by_precipitation[index][5])
 
     fig3, ax3 = plt.subplots()
-    ax3.scatter(top_10_precipitation, top_aqi)
-    ax3.set_xlabel("average precipitation")
-    ax3.set_ylabel("average aqi")
-    ax3.set_title("Average AQI vs Top 10 Average Precipitation (by State)")
+    ax3.scatter(top_50_precipitation, top_aqi)
+    ax3.set_xlabel("average weekly precipitation")
+    ax3.set_ylabel("average weekly aqi")
+    ax3.set_title("Average AQI vs Top 50 Average Precipitation (by State)")
 
     fig3.savefig("precipitation.png")
 
@@ -84,17 +84,17 @@ def create_precipitation_graph():
 def create_cloud_cover_graph():
     sort_by_cloud_cover = sorted(num_data, key = lambda x: x[4])
 
-    top_10_cloud_cover = []
+    top_50_cloud_cover = []
     top_aqi = []
-    for index in range(len(sort_by_cloud_cover) - 10, len(sort_by_cloud_cover)):
-        top_10_cloud_cover.append(sort_by_cloud_cover[index][4])
+    for index in range(len(sort_by_cloud_cover) - 50, len(sort_by_cloud_cover)):
+        top_50_cloud_cover.append(sort_by_cloud_cover[index][4])
         top_aqi.append(sort_by_cloud_cover[index][5])
 
     fig4, ax4 = plt.subplots()
-    ax4.scatter(top_10_cloud_cover, top_aqi)
-    ax4.set_xlabel("average cloud cover")
-    ax4.set_ylabel("average aqi")
-    ax4.set_title("Average AQI vs Top 10 Average Cloud Cover (by State)")
+    ax4.scatter(top_50_cloud_cover, top_aqi)
+    ax4.set_xlabel("average weekly cloud cover")
+    ax4.set_ylabel("average weekly aqi")
+    ax4.set_title("Average AQI vs Top 50 Average Cloud Cover (by State)")
 
     fig4.savefig("cloud_cover.png")
 
