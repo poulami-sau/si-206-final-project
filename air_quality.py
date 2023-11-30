8cea7e274ba2b200878f4252e4deacc6830807c0
Kate Chung <katechung@MacBook-Air-726.local>
2023-11-29 19:59:35 -0500

    fixed function format for air quality file

--- air_quality.py
+++ air_quality.py
@@ -5,12 +5,15 @@ import json
 from config import AIR_QUALITY_KEY
 from datetime import date
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-db_filename = dir_path + '/zipCode_airQuality.db'
-conn = sqlite3.connect(db_filename)
-cur = conn.cursor()
+def connect_database():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    db_filename = dir_path + '/zipCode_airQuality.db'
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
 
-def create_aq_table (date_today = date.today()):
+    return cur, conn
+
+def create_aq_table (date_today):
     cur.execute("CREATE TABLE IF NOT EXISTS air_quality_data (zip_code_id INTEGER, date_num INTEGER, aqi INTEGER)")
 
     cur.execute("SELECT MAX(zip_code_id) FROM air_quality_data WHERE date_num = ?", (date_today,))
@@ -47,5 +50,8 @@ def create_aq_table (date_today = date.today()):
                     (zip_code_id, today_day, aqi_num))
         conn.commit()
 
+
+    cur, conn = connect_database()
+    create_aq_table(cur, conn, date_today = date.today())
     cur.close()
     conn.close()
