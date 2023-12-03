0bbb253f1ac6eef0969193d96302ecdcc9ed1e57
poulami-sau <109125687+poulami-sau@users.noreply.github.com>
2023-12-03 16:10:37 -0500

    updated some code in weather_info and verified it still works

--- weather_info.py
+++ weather_info.py
@@ -23,38 +23,24 @@ def fetch_yesterday_hourly_response_data(url, params):
 def insert_data(cur, conn, url, params):
     date_yesterday = date.today() - timedelta(days = 1)
 
-    cur.execute("SELECT MAX(date_num) FROM weather_data")
-    date_yesterday_id = cur.fetchone()[0]
-
-    if date_yesterday_id == None:
-        date_yesterday_id = 1
-
-    cur.execute("SELECT MAX(zip_code_id), date_num FROM weather_data WHERE date_num = ?", (date_yesterday_id,))
-    max_zip_code_id, current_date = cur.fetchone()
+    cur.execute("SELECT COUNT(*) FROM weather_data JOIN dates ON weather_data.date_num = dates.day_id WHERE dates.date = ?", (date_yesterday,))
+    max_zip_code_id = cur.fetchone()[0]
 
     if max_zip_code_id == None:
         max_zip_code_id = 1
     elif max_zip_code_id < 100:
         max_zip_code_id += 1
     elif max_zip_code_id == 100:
-        cur.execute("SELECT date FROM dates WHERE day_id = ?", (date_yesterday_id,))
-        yesterday_date_table_str = cur.fetchone()[0]
-
-        if current_date != yesterday_date_table_str:
-            max_zip_code_id = 1
-
-            if date_yesterday_id < 7:
-                date_yesterday_id += 1
-            elif date_yesterday_id == 7:
-                exit()
-        else:
-            exit()
+        exit()
 
     cur.execute("SELECT * FROM zip_code_to_coordinates where zip_code_id = ?", (max_zip_code_id,))
     zip_code_id, latitude, longitude = cur.fetchone()
 
     params["latitude"] = latitude
     params["longitude"] = longitude
+
+    cur.execute("SELECT day_id FROM dates WHERE date = ?", (date_yesterday,))
+    yesterday_day_id = cur.fetchone()[0]
 
     yesterday_info_list = fetch_yesterday_hourly_response_data(url, params)
 
@@ -68,7 +54,7 @@ def insert_data(cur, conn, url, params):
         yesterday_cloud_cover = yesterday_info_list["cloud_cover"][hour]
 
         cur.execute("INSERT OR IGNORE INTO weather_data (zip_code_id, date_num, hour_gmt_val, temperature, relative_humidity, precipitation, cloud_cover) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                (zip_code_id, date_yesterday, yesterday_timestamp_hour, yesterday_temperature, yesterday_relative_humidity, yesterday_precipitation, yesterday_cloud_cover))
+                (zip_code_id, yesterday_day_id, yesterday_timestamp_hour, yesterday_temperature, yesterday_relative_humidity, yesterday_precipitation, yesterday_cloud_cover))
 
     conn.commit()
 
